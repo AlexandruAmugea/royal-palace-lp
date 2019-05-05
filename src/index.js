5e80e2b16a88cbdec90723213452ffe58b539be6
Alexandru Amugea <alexandruamugea@gmail.com>
2019-05-05 09:41:36 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import 'bootstrap'
+import './sass/main.scss'
+import './assets/flipclock.min'
+
+// TODO: replace the date with the real date
+const endDate = new Date("10 May 2019");
+const startDate = new Date();
+const remainingSeconds = Math.abs((endDate.getTime() - startDate.getTime()) / 1000);
+
+$('.clock').FlipClock(remainingSeconds, {countdown: true, clockFace: 'DailyCounter', showSeconds: false});
+
