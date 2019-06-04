1d00be21aa681596776a3b5acc3f9a7a09c26b48
Alexandru Amugea <alexandruamugea@gmail.com>
2019-06-04 14:30:42 +0300

    Typo fixes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './sass/main.scss'
 import './assets/flipclock.min'
 
 // TODO: replace the date with the real date
-const endDate = new Date("10 May 2019");
+const endDate = new Date("17 June 2019");
 const startDate = new Date();
 const remainingSeconds = Math.abs((endDate.getTime() - startDate.getTime()) / 1000);
 
