fd8fa4bd8ab880306d9e2194141f45d978fe2cf5
Alexandru Amugea <alexandruamugea@gmail.com>
2019-06-04 16:05:38 +0300

    Request for bonus, form error handling

--- src/index.js
+++ src/index.js
@@ -1,10 +1,43 @@
 import './sass/main.scss'
 import './assets/flipclock.min'
 
-// TODO: replace the date with the real date
+const $successMsg = $('#successMsg');
+const $errorMsg = $('#errorMsg');
+const $contactForm = $('#contact-form');
+const $clock = $('.clock');
+
+$successMsg.hide();
+$errorMsg.hide();
+
 const endDate = new Date("17 June 2019");
 const startDate = new Date();
 const remainingSeconds = Math.abs((endDate.getTime() - startDate.getTime()) / 1000);
 
-$('.clock').FlipClock(remainingSeconds, {countdown: true, clockFace: 'DailyCounter', showSeconds: false});
+$clock.FlipClock(remainingSeconds, {countdown: true, clockFace: 'DailyCounter', showSeconds: false});
+
+$contactForm.on('submit', (ev)=> {
+    const email = $('input[name="email"]');
+
+    ev.preventDefault();
+    ev.stopPropagation();
+
+    let success = (data)=> {
+      console.log(data);
+      if(data.status === 200) {
+          $contactForm.hide();
+          $successMsg.show();
+      } else {
+          $contactForm.hide();
+          $errorMsg.show();
+      }
+    };
+
+    $.ajax({
+        type: "POST",
+        url: 'http://localhost:3000/api/register',
+        data: email,
+        success: success,
+        dataType: "json"
+    });
+})
 
